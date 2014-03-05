13fd2a358b5c9136349f89f2a11377c8590c62ab
fins2thright <waltsmith@embarqmail.com>
2014-03-05 07:27:28 -0500

    Orientation fixes

---
+++ ObjectSongEngineMG/OSESpriteOrientation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ObjectSongEngineMG
+{
+    /// <summary>
+    /// Enumeration of the possible orientations of an OSESprite Graphic
+    /// </summary>
+    public enum OSESpriteOrientation
+    {
+        Up,
+        Right,
+        Down,
+        Left
+    }
+}
